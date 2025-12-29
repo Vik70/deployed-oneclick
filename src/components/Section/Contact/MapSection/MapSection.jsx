baa4917166c3f876d3dc50f4aa28fe8337e369a7
Vik <purvikbhatt16@gmail.com>
2025-12-29 22:45:47 +0000

    Fix contact page map embed

--- src/components/Section/Contact/MapSection/MapSection.jsx
+++ src/components/Section/Contact/MapSection/MapSection.jsx
@@ -1,12 +1,34 @@
-"use client"
+"use client";
+
+const ADDRESS_QUERY = "Mercury Centre, Central Way, Feltham TW14 0RN";
+const MAP_EMBED_URL = `https://maps.google.com/maps?q=${encodeURIComponent(
+  ADDRESS_QUERY
+)}&t=&z=16&ie=UTF8&iwloc=&output=embed`;
+
 const MapSection = () => {
-    return (
-        <section className="google-map-two">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2485.868557103386!2d-0.4132407230375551!3d51.460569714122926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876731017a2f93b%3A0x693bad99a6b6fad0!2sMercury%20centre%2C%20Central%20Way%2C%20Feltham%20TW14%200RN!5e0!3m2!1sen!2suk!4v1741213125343!5m2!1sen!2suk"
-                className="google-map-two__map" allowFullScreen></iframe>
-        </section>
-    );
+  return (
+    <section className="google-map-two">
+      <iframe
+        title="One Click Handling location on Google Maps"
+        src={MAP_EMBED_URL}
+        className="google-map-two__map"
+        loading="lazy"
+        allowFullScreen
+        referrerPolicy="no-referrer-when-downgrade"
+      />
+      <div className="google-map-two__link">
+        <a
+          href={`https://maps.google.com/?q=${encodeURIComponent(
+            ADDRESS_QUERY
+          )}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open in Google Maps
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default MapSection;
